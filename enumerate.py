2218ca681ccba358c88e03e7ab277d9c4af8b841
yogii786 <57557160+yogii786@users.noreply.github.com>
2019-11-25 22:42:46 +0530

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,7 @@
+#how to print both list values and indices
+
+data = ['red','green','blue','white']
+ 
+#lets use a built in function "enumerate"
+for index,value in enumerate(data):
+  print( '{}','{}'.format(index,value))
