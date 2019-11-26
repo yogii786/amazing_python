0f5d66aa38caa77ff3ba7aa4408a46fba58ad7b5
yogii786 <57557160+yogii786@users.noreply.github.com>
2019-11-26 20:27:01 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -4,4 +4,4 @@ data = ['red','green','blue','white']
  
 #lets use a built in function "enumerate"
 for index,value in enumerate(data):
-  print( '{}','{}'.format(index,value))
+  print( '{} {}'.format(index,value))
