af4ab131e063bcc7831d1d1eecdedb1b8e5205fd
yogesh bhati <57557160+yogii786@users.noreply.github.com>
2019-11-30 18:25:29 +0530

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -5,19 +5,19 @@ import matplotlib.pyplot as plt
 
 
 x= np.array([1,2,3,4,5])
-y= np.array([5,7,9,11,13])
+y= np.array([5,7,9,11,13])  # y = x*w + b   where w = weights or slope and b = bias , x = input/features, y = output/labels
 
 def gradient_descent(x,y):
-    a_initial = b_initial = 0
+    w_initial = b_initial = 0
     rate = 0.01
     n= len(x)
     plt.scatter(x,y)
     for i in range(100):
-        y_predicted = a_initial*x + b_initial
-        plt.plot(x, y_predicted , color = 'red')
-        md = -(2/n)*sum(x*(y-y_predicted))
-        yd = -(2/n)*sum(y-y_predicted)
-        a_initial = a_initial - rate * md
-        b_initial = b_initial - rate * yd
+        y_predicted = w_initial*x + b_initial
+        plt.plot(x, y_predicted , color = 'red')     # c = (1/n)*sum((y - y_predicted)**2)  this is known as cost function
+        wd = -(2/n)*sum(x*(y-y_predicted))  # partial derivative of cost function with respect to weight w
+        bd = -(2/n)*sum(y-y_predicted)      # partial derivative of cost function with respect to bias b
+        w_initial = w_initial - rate * wd
+        b_initial = b_initial - rate * bd
         
  gradient_descent(x,y)
